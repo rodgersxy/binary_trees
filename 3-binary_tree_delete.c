e8aa58ef3e6bea70ce6d2e5e5654ff042297c3ab
rodgersxy <rodgersny99@gmail.com>
2023-08-01 21:53:50 -0700

    update

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,14 +2,15 @@
 /**
  * binary_tree_delete - A function that deletes entire binary
  * tree
- * @tree - binary tree
+ * @tree: binary tree
  * Return: Void
  */
+
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return;
-	
+
 	/*post-order traversal to delete the tree*/
 	binary_tree_delete(tree->left);
 	binary_tree_delete(tree->right);
