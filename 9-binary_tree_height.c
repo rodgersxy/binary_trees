fa91fec849322df27418a6ff6b732e594a321dfc
rodgersxy <rodgersny99@gmail.com>
2023-08-02 02:51:23 -0700

    fixed bug

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,16 +13,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	if (tree->left)
+		left_height += 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right_height += 1 + binary_tree_height(tree->right);
 
 	if (left_height > right_height)
-	{
-		return (left_height + 1);
-	}
+		return (left_height);
 	else
-	{
-		return (right_height + 1);
-	}
+		return (right_height);
 }
